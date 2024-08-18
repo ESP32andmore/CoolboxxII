d222781d0e8c3e1acf1c46e32554a9412441f15d
ESP32andmore <fpovoski@esp32andmore.com>
2024-08-18 14:05:19 -0400

    Update program_coolboxx.sh

--- program_coolboxx.sh
+++ program_coolboxx.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p /home/user/coolboxx && cd /home/user/coolboxx
-wget https://github.com/ESP32andmore/CoolboxxII/raw/main/coolboxx2-factory-V1_0.bin
+wget https://github.com/ESP32andmore/CoolboxxII/raw/main/coolboxx2-factory-V1_1.bin
 wget https://github.com/ESP32andmore/CoolboxxII/raw/main/coolboxx2-firmware.sh
 chmod +x ./coolboxx2-firmware.sh
-./coolboxx2-firmware.sh ./coolboxx2-factory-V1_0.bin
+./coolboxx2-firmware.sh ./coolboxx2-factory-V1_1.bin
 cd /home/user
 rm -rf coolboxx
